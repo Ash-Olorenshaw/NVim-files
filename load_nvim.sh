efd74b98a6766ccc7ab5f8a19e37230f2130c652
Ash Olorenshaw <asholorenshaw@gmail.com>
2025-11-17 21:38:39 +1100

    Updated Php config and added Bash nvim loading script
    - Added a Bash config for loading Nvim configs from Bash rather than just PowerShell
    
    - Updated Php config to now use a different TS server so that it properly works with Vue again

---
+++ load_nvim.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+alias pnvim="export NVIM_APPNAME=\"nvim/nvim-Python\" && echo \"Neovim environment set for Python development\""
+alias onvim="export NVIM_APPNAME=\"nvim/nvim-Odin\" && echo \"Neovim environment set for Odin development\""
+alias fnvim="export NVIM_APPNAME=\"nvim/nvim-FSharp\" && echo \"Neovim environment set for F# development\""
+alias csnvim="export NVIM_APPNAME=\"nvim/nvim-CSharp\" && echo \"Neovim environment set for C# development\""
+alias jnvim="export NVIM_APPNAME=\"nvim/nvim-Java\" && echo \"Neovim environment set for Java development\""
+alias jsnvim="export NVIM_APPNAME=\"nvim/nvim-JavaScript\" && echo \"Neovim environment set for JavaScript development\""
+alias powernvim="export NVIM_APPNAME=\"nvim/nvim-Powershell\" && echo \"Neovim environment set for PowerShell development\""
+alias cnvim="export NVIM_APPNAME=\"nvim/nvim-C\" && echo \"Neovim environment set for C/C++ development\""
+alias phpnvim="export NVIM_APPNAME=\"nvim/nvim-Php\" && echo \"Neovim environment set for Php development\""
+alias cjnvim="export NVIM_APPNAME=\"nvim/nvim-Clojure\" && echo \"Neovim environment set for Clojure development\""
+alias nnvim="export NVIM_APPNAME=\"nvim/nvim-Nim\" && echo \"Neovim environment set for Nim development\""
+alias fornvim="export NVIM_APPNAME=\"nvim/nvim-Fortran\" && echo \"Neovim environment set for Fortran development\""
+alias testnvim="export NVIM_APPNAME=\"nvim/nvim-Testbed\" && echo \"Neovim environment set for testing plugins, etc\""
+alias unsetnvim="export NVIM_APPNAME=\"\" && echo \"Neovim environment successfully unset\""
+
--- nvim-Php/init.vim
+++ nvim-Php/init.vim
@@ -6,11 +6,11 @@ call plug#begin(g:plugin_location)
 	source ~/.config/nvim/common/cmp_setup.vim
 	source ~/.config/nvim/common/neotree_setup.vim
 
-	Plug 'pmizio/typescript-tools.nvim'
 	Plug 'vigoux/oak'
 call plug#end()
 
 lua << EOF
+	vim.g.markdown_fenced_languages = { "ts=typescript" }
 	TreesitterConfigure( { "php", "dockerfile" }, false )
 	require "nvim-treesitter.parsers".get_parser_configs().blade = {
 		install_info = {
@@ -21,9 +21,35 @@ lua << EOF
 		filetype = "blade"
 	}
 
-	vim.lsp.config("*", { capabilities = capabilities })
+	vim.lsp.config('*', { capabilities = vim.g.cmp_capabilities })
+	local vue_language_server_path = '/usr/local/lib/node_modules/@vue/language-server'
+	local vue_plugin = {
+		name = '@vue/typescript-plugin',
+		location = vue_language_server_path,
+		languages = { 'vue' },
+		configNamespace = 'typescript',
+	}
+	vim.lsp.config('vtsls', {
+		settings = {
+			vtsls = {
+				tsserver = {
+					globalPlugins = {
+						vue_plugin,
+					}
+				}
+			}
+		},
+		-- on_attach = function()
+		-- 	vim.lsp.enable('vue_ls')
+		-- end,
+		capabilities = vim.g.cmp_capabilities,
+		filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' }
+	})
+
+	vim.lsp.enable("cssls")
+	vim.lsp.enable("vtsls")
+	vim.lsp.enable("html")
 	vim.lsp.enable('intelephense')
-	vim.lsp.enable('vue_ls')
 
 	vim.diagnostic.config({ virtual_text = true, virtual_lines = { current_line = true }, })
 
